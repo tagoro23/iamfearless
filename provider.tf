4b7f4423848ca7b515caad5b5b93ba007d840e4d
tagoro23 <temitope.agoro16@gmail.com>
2023-01-05 14:01:51 -0500

    added archive provider

--- provider.tf
+++ provider.tf
@@ -2,7 +2,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "4.48.0"
+      version = "~>4.48.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
     }
   }
 }
